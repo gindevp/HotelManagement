d54887c1d42f625cc271e1dcfb09578800a7dd09
= <=>
2021-04-06 11:04:53 +0700

    hungnn

--- db/code.sql
+++ db/code.sql
@@ -15,17 +15,17 @@ DROP TABLE LOAIPHONG
 GO
 CREATE TABLE LOAIPHONG
 (
-	MALP		VARCHAR(10) PRIMARY KEY,
+	MALP		VARCHAR(10) NOT NULL PRIMARY KEY,
 	TENLP		NVARCHAR(50) NOT NULL UNIQUE,
 	MOTA		NVARCHAR(500) 
 )
 GO
 
 DELETE FROM LOAIPHONG
-INSERT LOAIPHONG VALUES ('STD', 'Standard', N'Phòng standard (viết tắt là STD) là loại phòng tiêu chuẩn, đơn giản nhất trong các khách sạn hiện nay. Đây là loại phòng có diện tích nhỏ, thường được đặt ở tầng thấp nhất và không có view hoặc view không được đẹp. Trang thiết bị của phòng standard cũng được khách sạn giảm tối thiểu. Chính vì vậy, giá phòng standard nằm ở mức thấp nhất trong các loại phòng khách sạn.', DEFAULT, DEFAULT, DEFAULT)
-INSERT LOAIPHONG VALUES ('SUP', 'Superior', N'Phòng superior cao cấp hơn phòng standard với diện tích lớn hơn (từ 20m2 trở lên) bao gồm 1-2 giường, tầm nhìn view cũng đẹp hơn. Trang thiết bị của phòng được khách sạn đầu tư hiện đại. Vì chất lượng tốt hơn nên mức giá cho phòng superior cũng sẽ cao hơn phòng standard.', DEFAULT, DEFAULT, DEFAULT)
-INSERT LOAIPHONG VALUES ('DLX', 'Deluxe', N'Phòng deluxe thường ở tầng trên cao với view đẹp (hướng ra núi, biển… ). Diện tích của loại phòng này rộng rãi hơn superior và được đầu tư trang thiết bị cao cấp như tivi, tủ lạnh, bồn rửa mặt cao cấp… Đương nhiên, mức giá niêm yết dành cho phòng deluxe sẽ cao hơn superior.', DEFAULT, DEFAULT, DEFAULT)
-INSERT LOAIPHONG VALUES ('SUT', 'Suite', N'Phòng suite là loại phòng cao cấp nhất khách sạn, được đặt ở tầng cao nhất, nơi có không gian thoáng đãng và không khí trong lành. Với diện tích từ 60 – 120m2, phòng suite thường bao gồm 1 phòng khách, 1 phòng ngủ riêng biệt, cửa sổ và ban công để khách ngắm phong cảnh.', DEFAULT, DEFAULT, DEFAULT)
+INSERT LOAIPHONG VALUES ('STD', 'Standard', N'Phòng standard (viết tắt là STD) là loại phòng tiêu chuẩn, đơn giản nhất trong các khách sạn hiện nay. Đây là loại phòng có diện tích nhỏ, thường được đặt ở tầng thấp nhất và không có view hoặc view không được đẹp. Trang thiết bị của phòng standard cũng được khách sạn giảm tối thiểu. Chính vì vậy, giá phòng standard nằm ở mức thấp nhất trong các loại phòng khách sạn.')
+INSERT LOAIPHONG VALUES ('SUP', 'Superior', N'Phòng superior cao cấp hơn phòng standard với diện tích lớn hơn (từ 20m2 trở lên) bao gồm 1-2 giường, tầm nhìn view cũng đẹp hơn. Trang thiết bị của phòng được khách sạn đầu tư hiện đại. Vì chất lượng tốt hơn nên mức giá cho phòng superior cũng sẽ cao hơn phòng standard.')
+INSERT LOAIPHONG VALUES ('DLX', 'Deluxe', N'Phòng deluxe thường ở tầng trên cao với view đẹp (hướng ra núi, biển… ). Diện tích của loại phòng này rộng rãi hơn superior và được đầu tư trang thiết bị cao cấp như tivi, tủ lạnh, bồn rửa mặt cao cấp… Đương nhiên, mức giá niêm yết dành cho phòng deluxe sẽ cao hơn superior.')
+INSERT LOAIPHONG VALUES ('SUT', 'Suite', N'Phòng suite là loại phòng cao cấp nhất khách sạn, được đặt ở tầng cao nhất, nơi có không gian thoáng đãng và không khí trong lành. Với diện tích từ 60 – 120m2, phòng suite thường bao gồm 1 phòng khách, 1 phòng ngủ riêng biệt, cửa sổ và ban công để khách ngắm phong cảnh.')
 GO
 SELECT * FROM LOAIPHONG
 
@@ -39,21 +39,18 @@ CREATE TABLE PHONG
 	SUCCHUA		INT NOT NULL CHECK(SUCCHUA > 0),
 	DONGIA		MONEY NOT NULL,
 	TRANGTHAI	BIT NOT NULL DEFAULT 0,
-	MALP		VARCHAR(10) NOT NULL,
-	ISACTIVE	BIT DEFAULT 1,
-	CREATEAT	DATE DEFAULT GETDATE(),
-	UPDATEAT	DATE DEFAULT GETDATE(),
+	MALP		VARCHAR(10) NOT NULL
 	FOREIGN KEY (MALP) REFERENCES LOAIPHONG(MALP) ON UPDATE CASCADE
 )
 GO
 
 DELETE FROM PHONG
-INSERT PHONG VALUES ('101', 4, 200000, DEFAULT, 'STD', DEFAULT, DEFAULT, DEFAULT)
-INSERT PHONG VALUES ('102', 4, 200000, DEFAULT, 'STD', DEFAULT, DEFAULT, DEFAULT)
-INSERT PHONG VALUES ('103', 2, 200000, DEFAULT, 'STD', DEFAULT, DEFAULT, DEFAULT)
-INSERT PHONG VALUES ('304', 4, 1000000, DEFAULT, 'DLX', DEFAULT, DEFAULT, DEFAULT)
-INSERT PHONG VALUES ('205', 4, 200000, DEFAULT, 'SUP', DEFAULT, DEFAULT, DEFAULT)
-INSERT PHONG VALUES ('206', 4, 200000, DEFAULT, 'SUP', DEFAULT, DEFAULT, DEFAULT)
+INSERT PHONG VALUES ('101', 4, 200000, DEFAULT, 'STD')
+INSERT PHONG VALUES ('102', 4, 200000, DEFAULT, 'STD')
+INSERT PHONG VALUES ('103', 2, 200000, DEFAULT, 'STD')
+INSERT PHONG VALUES ('304', 4, 1000000, DEFAULT, 'DLX')
+INSERT PHONG VALUES ('205', 4, 200000, DEFAULT, 'SUP')
+INSERT PHONG VALUES ('206', 4, 200000, DEFAULT, 'SUP')
 GO
 SELECT * FROM PHONG
 
@@ -64,18 +61,15 @@ GO
 CREATE TABLE BOPHAN
 (
 	MABP		VARCHAR(10) NOT NULL PRIMARY KEY,
-	TENBP		NVARCHAR(50) NOT NULL UNIQUE,
-	ISACTIVE	BIT DEFAULT 1,
-	CREATEAT	DATE DEFAULT GETDATE(),
-	UPDATEAT	DATE DEFAULT GETDATE()
+	TENBP		NVARCHAR(50) NOT NULL UNIQUE
 )
 GO
 
 DELETE FROM BOPHAN
-INSERT BOPHAN VALUES ('QL', N'Quản Lý', DEFAULT, DEFAULT, DEFAULT)
-INSERT BOPHAN VALUES ('KT', N'Kế Toán', DEFAULT, DEFAULT, DEFAULT)
-INSERT BOPHAN VALUES ('PB', N'Phòng Ban', DEFAULT, DEFAULT, DEFAULT)
-INSERT BOPHAN VALUES ('LT', N'Lễ Tân', DEFAULT, DEFAULT, DEFAULT)
+INSERT BOPHAN VALUES ('QL', N'Quản Lý')
+INSERT BOPHAN VALUES ('KT', N'Kế Toán')
+INSERT BOPHAN VALUES ('PB', N'Phòng Ban')
+INSERT BOPHAN VALUES ('LT', N'Lễ Tân')
 SELECT * FROM BOPHAN
 GO
 
@@ -95,17 +89,14 @@ CREATE TABLE NHANVIEN
 	EMAIL			VARCHAR(50) NOT NULL UNIQUE,
 	ANH				VARCHAR(50),
 	MABP			VARCHAR(10) NOT NULL,
-	ISACTIVE		BIT DEFAULT 1,
-	CREATEAT		DATE DEFAULT GETDATE(),
-	UPDATEAT		DATE DEFAULT GETDATE()
 	FOREIGN KEY (MABP) REFERENCES BOPHAN(MABP) ON UPDATE CASCADE
 )
 GO
 
 DELETE FROM NHANVIEN
-INSERT NHANVIEN VALUES ('PH01', N'Nguyễn Nhật Hùng', 'hungnn', '1998-03-05', 1, N'Hà Nội', '0984111111', 'hungnnph09719@fpt.edu.vn', DEFAULT, 'QL', DEFAULT, DEFAULT, DEFAULT)
-INSERT NHANVIEN VALUES ('PH02', N'Hứa Mạnh Hùng', 'hunghm', '2000-03-05', 1, N'Hà Nội', '0984111112', 'hunghmph09808@fpt.edu.vn', DEFAULT, 'KT', DEFAULT, DEFAULT, DEFAULT)
-INSERT NHANVIEN VALUES ('PH03', N'Nguyễn Hữu Quyết', 'quyetnh', '2001-03-05', 1, N'Hà Nội', '0984111113', 'quyetnhph12037@fpt.edu.vn', DEFAULT, 'LT', DEFAULT, DEFAULT, DEFAULT)
+INSERT NHANVIEN VALUES ('PH01', N'Nguyễn Nhật Hùng', 'hungnn', '1998-03-05', 1, N'Hà Nội', '0984111111', 'hungnnph09719@fpt.edu.vn', DEFAULT, 'QL')
+INSERT NHANVIEN VALUES ('PH02', N'Hứa Mạnh Hùng', 'hunghm', '2000-03-05', 1, N'Hà Nội', '0984111112', 'hunghmph09808@fpt.edu.vn', DEFAULT, 'KT')
+INSERT NHANVIEN VALUES ('PH03', N'Nguyễn Hữu Quyết', 'quyetnh', '2001-03-05', 1, N'Hà Nội', '0984111113', 'quyetnhph12037@fpt.edu.vn', DEFAULT, 'LT')
 SELECT * FROM NHANVIEN
 GO
 
@@ -133,18 +124,15 @@ CREATE TABLE DICHVU
 	TENDV			NVARCHAR(50) NOT NULL,
 	DONGIA			MONEY NOT NULL CHECK(DONGIA >= 0),
 	MOTA			NVARCHAR(500),
-	TRANGTHAI		BIT NOT NULL,
-	ISACTIVE		BIT DEFAULT 1,
-	CREATEAT		DATE DEFAULT GETDATE(),
-	UPDATEAT		DATE DEFAULT GETDATE()
+	TRANGTHAI		BIT NOT NULL
 )
 GO
 
 DELETE FROM DICHVU
 DBCC CHECKIDENT ('DICHVU', RESEED, 0)
-INSERT DICHVU VALUES ('Make-up room service', 200000, N'Make-up room service là dịch vụ làm phòng. Trong dịch vụ này, người làm phòng sẽ thay drap giường, khăn tắm và các đồ dùng trong phòng ngủ.', 1, DEFAULT, DEFAULT, DEFAULT)
-INSERT DICHVU VALUES ('Turndown service', 50000, N'Là dịch vụ chỉnh trang phòng buổi tối. Người làm phòng sẽ trải thẳng lại drap giường, gấp nếp giường, đặt cành hoa hoặc thiệp chúc ngủ ngon lên giường..', 1, DEFAULT, DEFAULT, DEFAULT)
-INSERT DICHVU VALUES ('Laundry service', 200000, N'Laundry service là dịch vụ giặt ủi trong khách sạn.', 1, DEFAULT, DEFAULT, DEFAULT)
+INSERT DICHVU VALUES ('Make-up room service', 200000, N'Make-up room service là dịch vụ làm phòng. Trong dịch vụ này, người làm phòng sẽ thay drap giường, khăn tắm và các đồ dùng trong phòng ngủ.', 1)
+INSERT DICHVU VALUES ('Turndown service', 50000, N'Là dịch vụ chỉnh trang phòng buổi tối. Người làm phòng sẽ trải thẳng lại drap giường, gấp nếp giường, đặt cành hoa hoặc thiệp chúc ngủ ngon lên giường..', 1)
+INSERT DICHVU VALUES ('Laundry service', 200000, N'Laundry service là dịch vụ giặt ủi trong khách sạn.', 1)
 SELECT * FROM DICHVU
 GO
 
@@ -161,18 +149,15 @@ CREATE TABLE KHACHHANG
 	SDT			VARCHAR(20) NOT NULL UNIQUE,
 	GIOITINH	BIT NOT NULL,
 	DIACHI		NVARCHAR(50) NOT NULL,
-	QUOCTICH	NVARCHAR(50) NOT NULL,
-	ISACTIVE	BIT DEFAULT 1,
-	CREATEAT	DATE DEFAULT GETDATE(),
-	UPDATEAT	DATE DEFAULT GETDATE()
+	QUOCTICH	NVARCHAR(50) NOT NULL
 )
 GO
 
 DELETE FROM KHACHHANG
 DBCC CHECKIDENT ('KHACHHANG', RESEED, 0)
-INSERT KHACHHANG VALUES (N'Khách hàng 1', '122122121', '0987654001', 1, N'Hà Nội', N'Việt Nam', DEFAULT, DEFAULT, DEFAULT)
-INSERT KHACHHANG VALUES (N'Khách hàng 2', '122122122', '0987654002', 0, N'Hồ Chí Minh', N'Việt Nam', DEFAULT, DEFAULT, DEFAULT)
-INSERT KHACHHANG VALUES (N'Khách hàng 3', '122122123', '0987654003', 1, N'Đà Nẵng', N'Việt Nam', DEFAULT, DEFAULT, DEFAULT)
+INSERT KHACHHANG VALUES (N'Khách hàng 1', '122122121', '0987654001', 1, N'Hà Nội', N'Việt Nam')
+INSERT KHACHHANG VALUES (N'Khách hàng 2', '122122122', '0987654002', 0, N'Hồ Chí Minh', N'Việt Nam')
+INSERT KHACHHANG VALUES (N'Khách hàng 3', '122122123', '0987654003', 1, N'Đà Nẵng', N'Việt Nam')
 GO
 SELECT * FROM KHACHHANG
 
@@ -185,17 +170,14 @@ CREATE TABLE NHACUNGCAP
 	MANCC		INT IDENTITY(1,1) PRIMARY KEY,
 	TENNCC		NVARCHAR(50) NOT NULL,
 	DIACHI		NVARCHAR(50) NOT NULL,
-	ISACTIVE	BIT DEFAULT 1,
-	CREATEAT	DATE DEFAULT GETDATE(),
-	UPDATEAT	DATE DEFAULT GETDATE()
 )
 GO
 
 DELETE FROM NHACUNGCAP
 DBCC CHECKIDENT ('NHACUNGCAP', RESEED, 0)
-INSERT NHACUNGCAP VALUES ('NCC01', N'Hà Nội', DEFAULT, DEFAULT, DEFAULT)
-INSERT NHACUNGCAP VALUES ('NCC02', N'Bắc Ninh', DEFAULT, DEFAULT, DEFAULT)
-INSERT NHACUNGCAP VALUES ('NCC03', N'Lạng Sơn', DEFAULT, DEFAULT, DEFAULT)
+INSERT NHACUNGCAP VALUES ('NCC01', N'Hà Nội')
+INSERT NHACUNGCAP VALUES ('NCC02', N'Bắc Ninh')
+INSERT NHACUNGCAP VALUES ('NCC03', N'Lạng Sơn')
 GO
 SELECT * FROM NHACUNGCAP
 
@@ -231,11 +213,10 @@ CREATE TABLE HDTHANHTOAN
 	NGAYTHUE	DATE NOT NULL,
 	NGAYTT		DATE NOT NULL DEFAULT GETDATE(),
 	TONGTIEN	MONEY NOT NULL CHECK (TONGTIEN > 0),
-	TRANGTHAI	BIT NOT NULL DEFAULT 0,
-	MAKH		INT,
+	MAKH		INT NOT NULL,
 	MANV		VARCHAR(10) NOT NULL
-	FOREIGN KEY (MAKH) REFERENCES KHACHHANG(MAKH),
-	FOREIGN KEY (MANV) REFERENCES NHANVIEN(MANV)
+	FOREIGN KEY (MAKH) REFERENCES KHACHHANG(MAKH) ON UPDATE CASCADE,
+	FOREIGN KEY (MANV) REFERENCES NHANVIEN(MANV) ON UPDATE CASCADE
 )
 GO
 
@@ -283,18 +264,15 @@ GO
 CREATE TABLE TIENNGHI
 (
 	MATN		INT IDENTITY(1,1) PRIMARY KEY,
-	TENTN		NVARCHAR(50) NOT NULL UNIQUE,
-	ISACTIVE	BIT DEFAULT 1,
-	CREATEAT	DATE DEFAULT GETDATE(),
-	UPDATEAT	DATE DEFAULT GETDATE()
+	TENTN		NVARCHAR(50) NOT NULL UNIQUE
 )
 GO
 DELETE FROM TIENNGHI
 DBCC CHECKIDENT ('TIENNGHI', RESEED, 0)
-INSERT TIENNGHI VALUES (N'Tivi', DEFAULT, DEFAULT, DEFAULT)
-INSERT TIENNGHI VALUES (N'Tủ lạnh', DEFAULT, DEFAULT, DEFAULT)
-INSERT TIENNGHI VALUES (N'Điều hòa', DEFAULT, DEFAULT, DEFAULT)
-INSERT TIENNGHI VALUES (N'Ghế massage', DEFAULT, DEFAULT, DEFAULT)
+INSERT TIENNGHI VALUES (N'Tivi')
+INSERT TIENNGHI VALUES (N'Tủ lạnh')
+INSERT TIENNGHI VALUES (N'Điều hòa')
+INSERT TIENNGHI VALUES (N'Ghế massage')
 GO
 SELECT * FROM TIENNGHI
 
@@ -307,7 +285,7 @@ CREATE TABLE PHONG_TIENNGHI
 	ID			INT IDENTITY(1,1) PRIMARY KEY,
 	SOPHONG		VARCHAR(10) NOT NULL,
 	MATN		INT NOT NULL,
-	SOLUONG		INT,
+	SOLUONG		INT NOT NULL,
 	TINHTRANG	NVARCHAR(50),
 	FOREIGN KEY (SOPHONG) REFERENCES PHONG(SOPHONG),
 	FOREIGN KEY (MATN) REFERENCES TIENNGHI(MATN),
---
+++ db/sqlcode.sql
@@ -0,0 +1,4 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [MABP]
+      ,[TENBP]
+  FROM [QLKS].[dbo].[BOPHAN]
